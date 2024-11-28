cb51ee9a1821082874b9dba1538d69b247c67ec9
Farah Najib <farahengnajib@gmail.com>
2024-11-28 13:45:26 +0100

    .

--- src/components/ProductDetailModal.tsx
+++ src/components/ProductDetailModal.tsx
@@ -37,11 +37,11 @@ const ProductDetailModal: React.FC<ProductDetailModalProps> = ({
             setIsLoading(false)
         }
     }
-     const decodeHtmlEntities = (text: string) => {
-         const element = document.createElement('div')
-         element.innerHTML = text
-         return element.textContent || element.innerText || ''
-     }
+    const decodeHtmlEntities = (text: string) => {
+        const element = document.createElement('div')
+        element.innerHTML = text
+        return element.textContent || element.innerText || ''
+    }
 
     useEffect(() => {
         if (show) {
